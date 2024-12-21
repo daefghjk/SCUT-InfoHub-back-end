0f99003489bd4e15150f215abe558c3be3968592
daefghjk <2105049655@qq.com>
2024-12-21 21:38:34 +0800

    修复迁移顺序

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -23,14 +23,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'wxcloudrun',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wxcloudrun',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
