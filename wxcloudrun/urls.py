10b4d6be1ec6bcb357b534619bec7c18645cb06f
daefghjk <2105049655@qq.com>
2025-01-03 21:01:28 +0800

    创建用户视图改为更新用户信息视图

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import PostViewSet, CommentViewSet, UserCreateView, LoginView, FanCountView, FanListView, unfollow_user, follow_user, UserCheckView
+from .views import PostViewSet, CommentViewSet, UserUpdateView, LoginView, FanCountView, FanListView, unfollow_user, follow_user, UserCheckView
 
 from rest_framework.routers import DefaultRouter
 
@@ -10,7 +10,7 @@ router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', UserCreateView.as_view(), name='user-list'),
+    path('users/', UserUpdateView.as_view(), name='user-update'),
     path('users/check/', UserCheckView.as_view(), name='user-check'),
     path('login/', LoginView.as_view(), name='login'),
     path('<str:name>/fan-count/', FanCountView.as_view(), name='fan-count'),
